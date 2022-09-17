b65cbc3d58b31389d077d3501d693f3dbbee97fd
do.pham1706 <do.pham1706@hcmut.edu.vn>
2022-09-18 00:53:34 +0700

    export word

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3.8
 COPY ./ /usr/questions_bank
 WORKDIR /usr/questions_bank
 RUN apt-get update && apt-get install -y wkhtmltopdf
+RUN apt-get install -y pandoc
 RUN pip install -r requirements.txt
--- app/api_views/image_and_file/convert_file.py
+++ app/api_views/image_and_file/convert_file.py
@@ -17,18 +17,22 @@ async def export_word(
     data: DATA_Export,
     # file: UploadFile = File(...,description="file as UploadFile"),
 ):
+    
     from htmldocx import HtmlToDocx
     import uuid    
     file_name = uuid.uuid4().hex
 
-    new_parser = HtmlToDocx()
-    # new_parser.parse_html_file('Questions.html', 'out')
-    # file.file.read()
-    docx = new_parser.parse_html_string(data.content)
-    docx.save(f'file_export/{file_name}.docx')
+    import pypandoc
+    pypandoc.convert_text(data.content, 'docx', format='html', outputfile=f'file_export/{file_name}.docx')
+    # new_parser = HtmlToDocx()
+    # # new_parser.parse_html_file('Questions.html', 'out')
+    # # file.file.read()
+    # docx = new_parser.parse_html_string(data.content)
+    # docx.save(f'file_export/{file_name}.docx')
     some_file_path = f'file_export/{file_name}.docx'
     background_tasks.add_task(remove_file, some_file_path)
     return FileResponse(some_file_path, media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document', filename='q.docx')
+
 
 
 @app.post("/export_pdf")
